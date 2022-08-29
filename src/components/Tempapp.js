3d09343b9b4b0b8d8a73ff45d2cb95dc9219bbd8
Bhuavaneshwari16 <bhavyabapanapally16@gmail.com>
2022-08-29 20:10:31 +0530

    weather

---
+++ src/components/Tempapp.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+// import App from "../App";
+import "./css/style.css";
+
+const Tempapp =() => {
+
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("mumbai");
+    useEffect( () =>{
+        const fetchApi = async () =>{
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=5097a176a6ef637720140b0f79f1d420`
+            const response = await fetch(url);
+
+            const resJson = await response.json();
+            setCity(resJson.main);
+
+            
+
+        }
+        fetchApi();
+    }, [search])
+
+
+    return(
+        <>
+        <div className="box">
+            <div className="inputData">
+                <input type="search" value={search} className="inputFeild" 
+                onChange={ (event)  => { setSearch(event.target.value)}}/>
+
+            </div>
+
+        
+        { !city ? (<p className="errorMsg">no data</p>) : (
+            <div>
+                <div className="info">
+                    <h2 className="location">
+                        <i className ="fas fa-street-view"></i>{search}
+                    </h2>
+                    <h1 className="temp">{city.temp} °Cel</h1>
+                    <h3 className="tempmin_max"> Min : {city.temp_min} °Cel | Max : {city.temp_max}°Cel
+                    </h3>
+                </div>
+                <div className= "wave -one"></div>
+                <div className= "wave -two"></div>
+                <div className= "wave -three"></div>
+
+            </div>
+
+        ) }
+     
+           
+
+        </div>
+        </>
+    )
+}
+export default Tempapp;
